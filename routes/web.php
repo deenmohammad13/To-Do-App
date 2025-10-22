67b8d8242cfb8216672e9b428904f27684168320
Deen Mohammad <deen9721@gmail.com>
2025-10-23 00:21:47 +0600

    Model Route and Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', action:[TodoController::class, 'index']);
+Route::post('/todos', action:[TodoController::class, 'postTodo']);
+Route::patch('/todos/status/{id}', action:[TodoController::class, 'updateStatus']);
+Route::get('/todos/update/{id}', action:[TodoController::class, 'edit']);
+Route::patch('/todos/update/{id}', action:[TodoController::class, 'updateTodo']);
+Route::patch('/todos/delete/{id}', action:[TodoController::class, 'deleteTodo']);
+
