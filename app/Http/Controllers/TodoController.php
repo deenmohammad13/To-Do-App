58fca4ac6ebc023c5cf3700f45a48bab9fcf6fbb
Deen Mohammad <deen9721@gmail.com>
2025-10-23 10:51:04 +0600

    post method in controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use Attribute;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class TodoController extends Controller
 {
@@ -12,5 +15,20 @@ class TodoController extends Controller
         $todos = Todo::latest()->get();
         return view(view: 'home', data: compact(var_name: 'todos'));
 
+    }
+
+    public function postTodo(Request $request): RedirectResponse
+    {
+        $image = null;
+        if(isset($request->picture)){
+            $image = time().'_'.$request->picture->getClientOriginalName().'.'.$request->picture->extension();
+            $request->picture->move(public_path(path: 'images'), $image);
+        };
+        Todo:: create(Attributes: [
+            'name' => $request->name,
+            'price' => $request->price,
+            'picture' => $image
+        ]);
+        return redirect(to: '/');
     }
 }
